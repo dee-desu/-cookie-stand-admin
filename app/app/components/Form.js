607d98b3e949a87ae047914467ca2f7b2465d28c
dee-desu <omardarweesh97@outlook.com>
2023-01-15 05:30:07 +0000

    v2

--- app/app/components/Form.js
+++ app/app/components/Form.js
@@ -1,15 +1,18 @@
 import React from "react";
-import { useState } from "react";
+import { useState ,useMemo} from "react";
+import { data } from "./data.js"; 
+import {Cookie_admin } from "../custom_hooks/TotalCol.js";
 
-export default function AdminForm() {
+export default function Form(props) {
   const [location, setLocation] = useState("");
   const [minCustomers, setMinCustomers] = useState("");
   const [maxCustomers, setMaxCustomers] = useState("");
   const [avgCookies, setAvgCookies] = useState("");
-  const [allOutputs, setAllOutputs] = useState([]);
+
 
   const handleLocation = (e) => {
     setLocation(e.target.value);
+    
   };
 
   const handleMinCustomers = (e) => {
@@ -25,14 +28,20 @@ export default function AdminForm() {
   };
   const handleSubmit = (e) => {
     e.preventDefault();
-    // const outpute = `{"location": "${location}","minCustomers":${minCustomers},"maxCustomers":${maxCustomers},"avgCookies":${avgCookies}}`;
+    
+  const cookie = Cookie_admin(data, location)
+  
     const output = {
-      _location: location || "None",
+      _location: location || "Amman",
       min_customers: minCustomers || "None",
       max_customers: maxCustomers || "None",
       avg_cookies: avgCookies || "None",
+      hourlySales: cookie || [48,42,30,24,42,24,36,42,42,48,36,42,24,36,516],
+      
     };
-    setAllOutputs([...allOutputs, output]);
+    props.getUserInput(output);
+  
+
   };
 
   return (
@@ -40,101 +49,63 @@ export default function AdminForm() {
       <section className=" flex justify-center ">
         <form
           onSubmit={handleSubmit}
-          class="relative flex flex-col break-words mb-6 rounded-lg bg-white-400 rounded-3xl shadow-2xl w-4/5"
+          className="relative flex flex-col break-words mb-6 rounded-lg bg-gray-400 rounded-3xl shadow-2xl w-4/5"
         >
           <div className="mt-7 mb-10 text-center border-b-2 ">
-            <span className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-300 bg-blue-600 py-4 px-5 shadow-lg">
+            <span className=" text-xl  text-white-800 rounded-3xl border-1 border-gray-300 bg-gray-300 py-4 px-5 shadow-lg">
               Create Cookie Stand
             </span>
           </div>
           <div className=" flex justify-center">
-            <label className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
+            <label className=" text-xl  text-white-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
               Location
             </label>
             <input
               type="text"
               name="location"
               onChange={handleLocation}
-              className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
+              className=" text-xl  text-white-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
             ></input>
           </div>
           <div className=" flex justify-center mt-1">
-            <label className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
+            <label className=" text-xl  text-white-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
               Minimum Customers per Hour
             </label>
             <input
               type="text"
               name="minCustomers"
               onChange={handleMinCustomers}
-              className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
+              className=" text-xl  text-white-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
             ></input>
           </div>
           <div className=" flex justify-center mt-1">
-            <label className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
+            <label className=" text-xl  text-white-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
               Maximum Customers per Hour
             </label>
             <input
               type="text"
               name="maxCustomers"
               onChange={handleMaxCustomers}
-              className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
+              className=" text-xl  text-white-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
             ></input>
           </div>
           <div className=" flex justify-center mt-1">
-            <label className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
+            <label className=" text-xl  text-white-800 rounded-3xl border-1 border-white-400 bg-gray-500 py-4 px-5 shadow-lg w-2/5 ml-2 pl-10">
               Average Cookies per Sale
             </label>
             <input
               type="text"
               name="avgCookies"
               onChange={handleAvgCookies}
-              className=" text-xl  text-gray-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
+              className=" text-xl  text-white-800 rounded-3xl border-1 border-gray-100 bg-gray-100 py-4 px-5 shadow-lg w-3/5 ml-20 pl-10 mr-2"
             ></input>
           </div>
           <div className="my-4 text-right mr-4 ">
-            <button className=" text-xl  text-gray-200 rounded-3xl border-1 border-indigo-200 bg-sky-700 py-4 px-5 shadow-lg hover:bg-sky-500">
-              Create 
+            <button className=" text-xl  text-white-200 rounded-3xl border-1 border-indigo-200 bg-sky-700 py-4 px-5 shadow-lg hover:bg-sky-500">
+              Create
             </button>
           </div>
         </form>
-      </section>
-      <section>
-        <table className=" items-center pt-20 pr-48 pb-24 pl-16 gap-24 border-solid ml-40 my-20 ">
-          <tr>
-            <th className="text-left p-3 text-xl bg-gray-700 text-white border-solid border-2 border-gray-400">
-              Location
-            </th>
-            <th className="text-left p-3 text-xl bg-gray-700 text-white border-solid border-2 border-gray-400">
-              Minimum Customers per Hour
-            </th>
-            <th className="text-left p-3 text-xl bg-gray-700 text-white border-solid border-2 border-gray-400">
-              Maximum Customers per Hour
-            </th>
-            <th className="p-3 text-xl bg-gray-700 text-white border-solid border-2 border-gray-400">
-              Average Cookies per Sale
-            </th>
-          </tr>
-
-          {allOutputs.length > 0 &&
-            allOutputs.map((item) => {
-              return (
-                <tr>
-                  <td className="p-3 text-xl text-gray-700 border-solid border-2 border-gray-400">
-                    {item._location}
-                  </td>
-                  <td className="p-3 text-xl text-gray-700 border-solid border-2 border-gray-400">
-                    {item.min_customers}
-                  </td>
-                  <td className="p-3 text-xl text-gray-700 border-solid border-2 border-gray-400">
-                    {item.max_customers}
-                  </td>
-                  <td className="p-3 text-xl text-gray-700 border-solid border-2 border-gray-400">
-                    {item.avg_cookies}
-                  </td>
-                </tr>
-              );
-            })}
-        </table>
       </section>
     </div>
   );
