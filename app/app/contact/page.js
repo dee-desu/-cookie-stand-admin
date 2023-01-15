c5b64647c8d00c1f5f117b30312d670d7b2425f8
dee-desu <omardarweesh97@outlook.com>
2023-01-15 03:58:41 +0000

    v1

---
+++ app/app/contact/page.js
@@ -0,0 +1,10 @@
+import React from "react";
+
+export default function About(){
+    return (
+        <div className="w-screen h-screen flex justify-center place-items-center">
+            <p className="text-4xl">contact us on: Facebook</p>
+            <br/>
+        </div>
+    )
+}
